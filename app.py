cacd505041cd1c9d99cd34bf2701e3b860009ca3
Douglas Silva <douglasitpro@gmail.com>
2025-01-15 19:35:28 -0300

    Ajustes

--- app.py
+++ app.py
@@ -1,23 +1,21 @@
-import streamlit as st 
+import streamlit as st
 from views.home_views import home
 from views.app1_views import app1
 from views.app2_views import app2
 from views.login_views import login
 from views.register_views import register
 
-#inicializando estado autenticação
+# Inicializando estado de autenticação
 if "authenticated" not in st.session_state:
-    st.session_state.authenticated  = False
+    st.session_state.authenticated = False
     st.session_state.username = ""
-    st.session_state.show_register = False #adiciona controle para alterarnar login e registro
-   
+    st.session_state.show_register = False  # Adiciona controle para alternar login e registro
 
-def set_show_register(value):                    # Alterna entre login e registro
+def set_show_register(value):  # Alterna entre login e registro
     st.session_state.show_register = value
-    st.rerun()                                # não funciona: st.experimental_rerun()
+    st.rerun()
 
-
-#função que gerencia login e registro
+# Função que gerencia login e registro
 def auth_page():
     if st.session_state.show_register:
         register()  # Renderiza a view de registro
@@ -28,30 +26,28 @@ def auth_page():
         if st.button("Não tem uma conta? Registre-se"):
             set_show_register(True)
 
-
-
 # Verifica a autenticação
 if not st.session_state.authenticated:
-    auth_page()                                  #Renderiza login ou registro
+    auth_page()  # Renderiza login ou registro
 else:
-    #Barra Lateral de navegação:
+    # Barra Lateral de navegação:
     st.sidebar.title(f"Bem Vindo, {st.session_state.username}")
-    page = st.sidebar.radio("Navegação", ["Home", "App1", "App2"])      #botão de navegação
+    page = st.sidebar.radio("Navegação", ["Home", "App1", "App2"])  # Botão de navegação
     logout_button = st.sidebar.button("Logout")
 
-    #botão de logout
+    # Botão de logout
     if logout_button:
-        st.session_state.authenticated =False
+        st.session_state.authenticated = False
         st.session_state.username = ""
+        st.success("Logout efetuado com sucesso.") # Mensagem de feedback
 
-        # Atribuir diretamente os estados para "resetar" a interface
-        st.experimental_set_query_params()  # Usado para "recarregar" sem conflitos
+        # Limpa os parâmetros da URL usando st.query_params
+        st.rerun()
 
-
-    # Renderiza páginas, confome a escolha no botão radio
+    # Renderiza páginas, conforme a escolha no botão radio
     if page == "Home":
         home()
     elif page == "App1":
         app1()
     elif page == "App2":
-        app2
+        app2()
