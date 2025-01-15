6e5a754728dea990e147b8b1e3f1e559d33e84a8
Douglas Silva <douglasitpro@gmail.com>
2025-01-15 12:02:15 -0300

    Criado o aqruivo de models para usuários |  Adicionando função de add usuario

---
+++ models/user.py
@@ -0,0 +1,22 @@
+import bcrypt
+from config.database import get_connection
+
+def create_user(usename, password):
+    connection = get_connection()
+    if not connection:
+        return False
+    
+    hasshed_password = bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt())
+
+    try:
+        cursor = connection.cursor()
+        query = "INSERT INTO users (username, password) VALUES (%s, %s)"
+        cursor.execute(query, hasshed_password.decode('utf8'))               #chamando a variavel para hash de senha
+        connection.commit()
+        return True
+
+    except Exception as e:
+        print(f"Erro ao criar o usuário: {e}")
+        return False
+    finally:
+        connection.close()
