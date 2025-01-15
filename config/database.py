6bccc5860d2cba289615923cb87d9854d666ebdc
Douglas Silva <douglasitpro@gmail.com>
2025-01-15 11:09:21 -0300

    Ajuste adicição de port

--- config/database.py
+++ config/database.py
@@ -1,6 +1,6 @@
 import mysql.connector
 from mysql.connector import Error
-from config.settings import DB_HOST, DB_USER, DB_PASSWORD, DB_NAME, DB_PORT
+from settings import DB_HOST, DB_USER, DB_PASSWORD, DB_NAME, DB_PORT
 
 def get_connection():
     try:
@@ -11,6 +11,10 @@ def get_connection():
             database = DB_NAME,
             port = DB_PORT
         )
+        
         return connection
+        
     except Error as e:
         print(f"Erro ao conectar ao MySQL: {e}")
+    
+   
